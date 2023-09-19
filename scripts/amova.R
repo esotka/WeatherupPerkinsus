25259c2cb5bd5e1f0a05a19d861f65785cc0f95d
esotka <eriksotka@gmail.com>
2023-09-19 13:52:04 -0400

    Changes from local computer

---
+++ scripts/amova.R
@@ -0,0 +1,60 @@
+### AMOVA on mpgl
+
+library(pegas)
+rm(list=ls())
+meta <- read.csv('./meta_36ind_final.csv')
+# check meta$file==rownames(gprob) ALL TRUE
+gprob<- read.table('./virginica_36ind/cvirg_36ind_52458snp.mpgl.txt')
+rownames(gprob) = meta$file
+ids <- rownames(gprob) 
+pop <- factor(paste(meta$state,meta$VA_reg))
+reg <- factor(meta$reg)
+st.d <- dist(gprob)## distances between rows
+print(m1 <- amova(st.d ~ reg/pop,nperm=1000))
+sig2 <- setNames(m1$varcomp$sigma2,rownames(m1$varcomp))
+print(getPhi(sig2))
+print(m1 <- amova(st.d ~ reg,nperm=1000))
+sig2 <- setNames(m1$varcomp$sigma2,rownames(m1$varcomp))
+print(getPhi(sig2))
+print(m1 <- amova(st.d ~ pop,nperm=1000))
+sig2 <- setNames(m1$varcomp$sigma2,rownames(m1$varcomp))
+print(getPhi(sig2))
+
+### pairwise PhiST (AL, LA, VA_easternShore, VA_Bay)
+pairs_to_use = combn(levels(pop),2)
+outStats = c()
+for (i in 1:ncol(pairs_to_use))
+{
+gprob2 <- gprob[pop%in%c(pairs_to_use[1,i],pairs_to_use[2,i]),]
+ids2 <- rownames(gprob2)
+pop2 <- factor(paste(meta$state,meta$VA_reg)[match(ids2,meta$file)])
+st.d2 <- dist(gprob2)## distances between rows
+m1 <- amova(st.d2 ~ pop2,nperm=1000)
+p = round(m1$varcomp[1,2],3)
+sig2 <- setNames(m1$varcomp$sigma2,rownames(m1$varcomp))
+phist = round(getPhi(sig2)[1],3)
+outStats = rbind(outStats,data.frame(pop1=pairs_to_use[1,i],pop2=pairs_to_use[2,i],phist,p))
+}
+
+### pairwise PhiST (Gulf, VA_easternShore, VA_Bay)
+pop_v2 = as.character(pop)
+pop_v2[pop_v2%in%c("LA ","AL ")] = "Gulf"; pop_v2 = factor(pop_v2)
+print(m1 <- amova(st.d ~ pop_v2,nperm=1000))
+sig2 <- setNames(m1$varcomp$sigma2,rownames(m1$varcomp))
+print(getPhi(sig2))
+
+pairs_to_use = combn(levels(pop_v2),2)
+for (i in 1:ncol(pairs_to_use))
+{
+gprob2 <- gprob[pop_v2%in%c(pairs_to_use[1,i],pairs_to_use[2,i]),]
+ids2 <- rownames(gprob2)
+pop2 <- factor(paste(meta$state,meta$VA_reg)[match(ids2,meta$file)])
+st.d2 <- dist(gprob2)## distances between rows
+m1 <- amova(st.d2 ~ pop2,nperm=1000)
+p = round(m1$varcomp[1,2],3)
+sig2 <- setNames(m1$varcomp$sigma2,rownames(m1$varcomp))
+phist = round(getPhi(sig2)[1],3)
+outStats = rbind(outStats,data.frame(pop1=pairs_to_use[1,i],pop2=pairs_to_use[2,i],phist,p))
+}
+
+print(outStats)
---
+++ scripts/ngsAdmix.pretty.R
@@ -0,0 +1,55 @@
+rm(list=ls())
+ks <- c("K2","K3","K4","K5","K6")#,"K7","K8","K9","K10")
+
+############# ENV DATA ##############
+meta = read.csv("../meta_36ind_final.csv")
+state_reg = factor(paste(meta$state,meta$VA_reg))
+state_reg_ordered = state_reg[order(state_reg)]
+
+kcol <- c("black", #1
+          "red",#2
+          "darkgreen",#3
+          "gainsboro",#4
+          "brown",#5
+          "deepskyblue",#6
+          "yellow",#7
+          "dodgerblue4",#8
+          "darkorchid",#9
+          "burlywood")##10
+colorder <- list(
+  c(1,2), #ks=2
+  c(1,2,3), #ks=3
+  c(2,1,3,4), #ks=4
+  c(4,2,1,3,5), #ks=5
+  c(3,2,1,4,5,6), #ks=6
+  c(2,5,3,7,1,6,4), #ks=7
+  c(4,7,6,1,2,3,5,8), #ks=8
+  c(4,8,3,9,6,5,2,1,7), #ks=9
+  c(9,1,2,8,4,6,5,7,10,3)) #ks=10
+
+pdf('ngsAdmix.pretty.pdf',width=5,height=4)
+par(mfrow=c(11,1),mar=c(0,0,0,0),xpd = TRUE)
+
+for (k in 1:length(ks))
+{
+  dat <- read.delim(paste(ks[k],"run1.qopt",sep=""),sep=" ",header = F)
+  dat <- dat[,-(dim(dat)[2])] # remove extra column
+  dat <- dat[,order(colorder[[k]])]
+  dat <- dat[order(state_reg),]
+  fig <- barplot(t(dat),col=kcol[1:length(colorder[[k]])],space=0,border=NA,xlab="",ylab="",
+          names.arg = rep("",nrow(dat)),horiz=F)#,main=substr(ks[k],1,3))
+  mtext(substr(ks[k],1,3),side=2,line=-1.5,at=.5,cex=0.7)
+  x <- 1:dim(dat)[1]
+  for(i in 1:length(levels(state_reg_ordered)))
+    {
+  xtmp <- x[state_reg_ordered==levels(state_reg_ordered)[i]]
+  segments(max(xtmp),1,max(xtmp),-.05,col="white",lwd=2)
+  mtext(side=1,at=mean(xtmp)-0.5,levels(state_reg)[i],cex=.5,line=.5)
+  }
+}
+xtick<-c(0,3,12,30,36)
+axis(side=1, at=xtick, labels = FALSE)
+ 
+    
+dev.off()
+
